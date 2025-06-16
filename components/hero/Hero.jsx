eb95383a6d97ec5bba364e894621a111f34b96d4
Kibe <kiberichard.kr@gmail.com>
2025-06-16 18:25:46 +0300

    Responsive Hero Section

---
+++ components/hero/Hero.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import heroImg from "@/public/speak-hero-4.png";
+import styles from "./Hero.module.css";
+import { ArrowRight } from "lucide-react";
+
+export default function Hero() {
+  return (
+    <section className={styles.section}>
+      <div className={`container ${styles.heroContainer}`}>
+        <header className={styles.header}>
+          <p>Find Your Family</p>
+          <h1>Where Silence Finds A Voice</h1>
+          <Link href="#" className={styles.getStartedBtn}>
+            Get Started <ArrowRight size={18} />
+          </Link>
+        </header>
+
+        <main className={styles.main}>
+          <Image
+            src={heroImg}
+            alt="Hero Image"
+            className={styles.heroImage}
+            priority
+            placeholder="blur"
+          />
+        </main>
+      </div>
+    </section>
+  );
+}
